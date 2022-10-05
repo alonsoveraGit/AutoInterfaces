3e2ac450e4bc9ce146edc5174aa68b6d692a648a
alonsoveraGit <alonsoverajimenez@gmail.com>
2022-10-05 17:40:58 +0200

    add ntpdate and sync time

--- network.py
+++ network.py
@@ -2,6 +2,16 @@
 from collections import defaultdict
 from os import system
 #import os as linux
+
+#Actualizamos zona horaria:
+
+try:
+    system("/usr/sbin/ntpdate hora.cica.es > /dev/null 2>&1 && /usr/bin/hwclock -s")
+
+except:
+    system("apt update && apt install ntpdate -y")
+    system("/usr/sbin/ntpdate hora.cica.es > /dev/null 2>&1 && /usr/bin/hwclock -s")
+
 try:
     import netifaces
 except ModuleNotFoundError as e:
@@ -15,8 +25,8 @@ except ModuleNotFoundError as e:
     from pyroute2 import IPRoute
 
 my_gateways =defaultdict(dict)
-my_gateways['192.168.58.1']="192.168.58.175/24"
-my_gateways['192.168.10.1']="192.168.10.20/24"
+my_gateways['192.168.58.1']="192.168.58.244/24"
+my_gateways['192.168.20.1']="192.168.20.244/24"
 
 def get_gateway():
     gws = netifaces.gateways()
@@ -34,6 +44,7 @@ if(my_gateways.__contains__(gateway)):
     ip= getipfromgateway(gateway)
     print(f"Asignando IP: {ip}")
     setIpAddr('ens33', ip,gateway)
+    system("date")
 else:
     print("Localizacion no reconocida.")
     print("Alonso")
